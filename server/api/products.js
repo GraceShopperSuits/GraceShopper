33f23084ec1bc993d03157e99c00e7b8102e5bbc
Nabil Yafai <nabilyafai90@gmail.com>
2018-06-05 16:49:20 -0400

    added category route

--- server/api/products.js
+++ server/api/products.js
@@ -9,8 +9,17 @@ router.get('/', (req, res, next) => {
     .catch(next)
 })
 
-//GET route for '/api/products/:productId' -- serves one product by ID
-router.get('/:productId', (req, res, next) => {
+//GET route for '/api/products/:productType' -- serves all products in that type/category
+router.get('/:productType', (req, res ,next) => {
+    Product.findByType(req.params.productType)
+    .then(products => {
+        res.json(products);
+    })
+    .catch(next);
+})
+
+//GET route for '/api/products/SKU/:productId' -- serves one product by ID
+router.get('/SKU/:productId', (req, res, next) => {
     Product.findById(req.params.productId)
       .then(product => res.json(product))
       .catch(next)
