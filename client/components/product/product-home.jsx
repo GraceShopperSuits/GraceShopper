4a6ca9f9c5f1b65346dc8026e965ea9f82813f7b
Donghao Wu <donghao.wu.us@gmail.com>
2018-06-05 15:37:10 -0400

    finish productLanding page

---
+++ client/components/product/product-home.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { NavLink } from "react-router-dom"
+import { connect } from 'react-redux'
+
+class ProductLanding extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        const products = this.props.products;
+        return (
+            <div className='Landing'>
+                {
+                    products.map(product => {
+                        return (
+                            <div className='SingleProduct' key={product.id}>
+                                <div className='ProductText'>
+                                    <img scr={product.imgUrl} />
+                                    <span>{product.name}</span>
+                                    <small>{product.color}</small>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+}
+
+const mapState = state=>{
+    return{
+        products:state.products
+    }
+}
+
+export default connect(mapState,null)(ProductLanding)
