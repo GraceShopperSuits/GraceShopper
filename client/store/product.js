cb00deb626d79934733526da3e3e1a51bd4d9341
Donghao Wu <donghao.wu.us@gmail.com>
2018-06-05 16:50:17 -0400

    finish productHome and productReducer

---
+++ client/store/product.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+const INITIALIZE = 'INITIALIZE_PRODUCTS'
+
+const init = (products) => {
+    return {
+        type: INITIALIZE,
+        products
+    }
+}
+
+const reducer = (state = [], action) => {
+    switch (action.type) {
+        case INITIALIZE: {
+            return action.product
+        }
+        default: {
+            return state
+        }
+    }
+}
+
+export default reducer;
+
+export const fetchProduct = ()=>{
+    return (dispatch)=>{
+        axios.get('')
+    }
+}
+
