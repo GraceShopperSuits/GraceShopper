7fae1130b6055c898a1a7f1b9f76bfa82484163f
Nabil Yafai <nabilyafai90@gmail.com>
2018-06-06 15:47:13 -0400

    added post route for all products, will need to add protection later to only allow admins to add a product, anyone with route can add products

--- server/api/products.js
+++ server/api/products.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { Product, Price } = require('../db/models')
+const { Product, Price, Option } = require('../db/models')
 module.exports = router
 
 //GET route for '/api/products' -- serves all products
@@ -30,4 +30,30 @@ router.get('/SKU/:productId', (req, res, next) => {
   })
     .then(product => res.json(product))
     .catch(next)
+})
+
+//POST route for '/api/products' -- Allows admin to add a product
+router.post('/', (req, res, next) => {
+  Product.create({
+    name: req.body.name,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    season: req.body.season,
+    type: req.body.type,
+  })
+    .then(async product => {
+      const price = await Price.create({ cost: Number(req.body.cost) })
+      const option = await Option.create({
+        size: req.body.size,
+        color: req.body.color,
+        fit: req.body.fit,
+        quantity: req.body.quantity,
+      })
+      await product.addPrice(price)
+      await product.addOption(option)
+      return product
+    })
+    .then(updatedProduct => {
+      res.json(updatedProduct)
+    })
 })
