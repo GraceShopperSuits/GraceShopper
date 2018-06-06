b02d552927965565388892fce402d8dd70bedc47
Jack Lye <catncradle@gmail.com>
2018-06-06 15:44:37 -0400

    added tick bar for categorization

--- client/components/product/product-home.js
+++ client/components/product/product-home.js
@@ -5,17 +5,64 @@ import { connect } from 'react-redux'
 class ProductLanding extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      shoes: false,
+      shirts: false,
+      suits: false,
+    }
+    this.toggleCheckboxChange = this.toggleCheckboxChange.bind(this)
   }
-  componentDidMount() {
-    // store.getState()
+  toggleCheckboxChange(event) {
+    this.setState({ [event.target.name]: !this.state[event.target.name] })
+    console.log(event.target.name, 'here', this.state)
   }
   //need api routes to Link
   render() {
     const products = this.props.products || []
-    console.log(this.props)
+
+    const shoes = products.filter(product => {
+      return product.type === 'shoe'
+    })
+    const shirts = products.filter(product => {
+      return product.type === 'shirt'
+    })
+    const suits = products.filter(product => {
+      return product.type === 'suit'
+    })
+    console.log('prod', products)
+    console.log('state', this.state)
+    // seperating products by type
     return (
       <div className="Landing">
+        <div className="checkbox">
+          <label>
+            <input
+              type="checkbox"
+              name="suits"
+              //   checked={isChecked}
+              onChange={this.toggleCheckboxChange}
+            />
+            suits
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              name="shirts"
+              //   checked={isChecked}
+              onChange={this.toggleCheckboxChange}
+            />
+            shirts
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              name="shoes"
+              //   checked={isChecked}
+              onChange={this.toggleCheckboxChange}
+            />
+            shoes
+          </label>
+        </div>
         {products.map(product => {
           return (
             <div className="SingleProduct" key={product.id}>
