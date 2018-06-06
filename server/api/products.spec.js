4482e2d1f2eb04271be5d8d2ccfaf0e82381c4a4
Raymond Qi <raymond@Raymonds-MacBook-Air.local>
2018-06-06 11:46:11 -0400

    added Tests for Product API and Model

--- server/api/products.spec.js
+++ server/api/products.spec.js
@@ -1,1 +1,35 @@
-//test routes for products
+const { expect } = require('chai')
+const request = require('supertest')
+const db = require('../db')
+const app = require('../index')
+const Product = db.model('product')
+
+describe('Product routes', () => {
+    beforeEach(() => {
+        return db.sync({ force: true })
+    })
+
+    describe('/api/products/', () => {
+
+        beforeEach(() => {
+            return Product.create({
+                name: 'Preet',
+                description: 'this is a shoe',
+                imageUrl: 'placeholder',
+                season: 'summer',
+                type: 'shoe'
+            })
+        })
+
+        it('GET /api/products', () => {
+            return request(app)
+                .get('/api/products')
+                .expect(200)
+                .then(res => {
+                    expect(res.body).to.be.an('array')
+                    expect(res.body[0].name).to.be.equal('Preet')
+                    expect(res.body[0].description).to.be.equal('this is a shoe')
+                })
+        })
+    }) // end describe('/api/users')
+}) // end describe('User routes')
--- server/db/models/product.js
+++ server/db/models/product.js
@@ -4,15 +4,15 @@ const db = require('../db')
 const Product = db.define('product', {
   name: {
     type: Sequelize.STRING,
+    allowNull: false
   },
   description: {
     type: Sequelize.TEXT,
+    allowNull: false
   },
   imageUrl: {
     type: Sequelize.STRING,
-  },
-  color: {
-    type: Sequelize.STRING,
+    allowNull: false
   },
   season: {
     type: Sequelize.STRING,
