97f842c0a62fbc29e7c47f5bde7057cc52e0072c
Nabil Yafai <nabilyafai90@gmail.com>
2018-06-06 21:05:30 -0400

    single product options no longer display duplicates

--- client/components/product/product.js
+++ client/components/product/product.js
@@ -5,7 +5,17 @@ import { connect } from 'react-redux'
 class Product extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      color: '',
+      fit: '',
+      size: '',
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    })
   }
   render() {
     const id = Number(this.props.match.params.productId)
@@ -19,7 +29,30 @@ class Product extends Component {
     const optionsInStock = options.filter(product => {
       return product.quantity > 0
     })
-    console.log(prices)
+    const colorsInStock = Array.from(
+      new Set(
+        optionsInStock.map(option => {
+          return option.color
+        })
+      )
+    )
+    const fitsInStock = Array.from(
+      new Set(
+        optionsInStock.map(option => {
+          return option.fit
+        })
+      )
+    )
+    const sizesInStock = Array.from(
+      new Set(
+        optionsInStock.map(option => {
+          return option.size
+        })
+      )
+    )
+
+    console.log('optionsz', colorsInStock)
+    console.log('state,', this.state)
     return (
       <div className="ProductComponent">
         {singleProduct.id ? (
@@ -30,22 +63,23 @@ class Product extends Component {
             <h3>{singleProduct.description}</h3>
             <h3>{singleProduct.season} collection</h3>
             <h3>Price: {prices[prices.length - 1].cost}</h3>
-            <label htmlFor="sizes">Select a color: </label>
-            <select name="colors">
-              {optionsInStock.map(option => {
-                return <option key={option.id}> {option.color} </option>
+            <label htmlFor="color">Select a color: </label>
+            <select name="color" onChange={this.handleChange}>
+              <option value="">Pick a color</option>
+              {colorsInStock.map(color => {
+                return <option key={color}> {color} </option>
               })}
             </select>
-            <label htmlFor="fits">Select a fit: </label>
-            <select name="fits">
-              {optionsInStock.map(option => {
-                return <option key={option.id}> {option.fit} </option>
+            <label htmlFor="fit">Select a fit: </label>
+            <select name="fit" onChange={this.handleChange}>
+              {fitsInStock.map(fit => {
+                return <option key={fit}> {fit} </option>
               })}
             </select>
-            <label htmlFor="sizes">Select a size: </label>
-            <select name="sizes">
-              {optionsInStock.map(option => {
-                return <option key={option.id}> {option.size} </option>
+            <label htmlFor="size">Select a size: </label>
+            <select name="size">
+              {sizesInStock.map(size => {
+                return <option key={size}> {size} </option>
               })}
             </select>
           </div>
