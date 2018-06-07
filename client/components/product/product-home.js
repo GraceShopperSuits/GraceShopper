606bb92f67b6c720282fce8017cba55417d2dbca
Nabil Yafai <nabilyafai90@gmail.com>
2018-06-07 16:27:40 -0400

    added more filter functionality to all products, for season and color

--- client/components/product/product-home.js
+++ client/components/product/product-home.js
@@ -8,8 +8,14 @@ class ProductLanding extends Component {
     this.state = {
       spring: false,
       summer: false,
-      fall: false,
+      autumn: false,
       winter: false,
+      black: false,
+      navy: false,
+      maroon: false,
+      pink: false,
+      white: false,
+      brown: false,
     }
     this.toggleCheckboxChange = this.toggleCheckboxChange.bind(this)
   }
@@ -21,83 +27,135 @@ class ProductLanding extends Component {
     const products = this.props.products || []
 
     const summer = products.filter(product => {
-      return product.season === 'summer' && this.state.summer
+      return product.season === 'Summer' && this.state.summer
     })
     const spring = products.filter(product => {
-      return product.season === 'spring' && this.state.spring
+      return product.season === 'Spring' && this.state.spring
     })
-    const fall = products.filter(product => {
-      return product.season === 'fall' && this.state.fall
+    const autumn = products.filter(product => {
+      return product.season === 'Autumn' && this.state.autumn
     })
     const winter = products.filter(product => {
-      return product.season === 'winter' && this.state.winter
+      return product.season === 'Winter' && this.state.winter
+    })
+    const black = products.filter(product => {
+      return product.color === 'Black' && this.state.black
+    })
+    const navy = products.filter(product => {
+      return product.color === 'Navy' && this.state.navy
+    })
+    const maroon = products.filter(product => {
+      return product.color === 'Maroon' && this.state.maroon
+    })
+    const pink = products.filter(product => {
+      return product.color === 'Pink' && this.state.pink
+    })
+    const white = products.filter(product => {
+      return product.color === 'White' && this.state.white
+    })
+    const brown = products.filter(product => {
+      return product.color === 'Brown' && this.state.brown
     })
 
-    this.filteredProducts = [...summer, ...winter, ...fall, ...spring]
-
+    this.filteredProducts = [
+      ...summer,
+      ...winter,
+      ...autumn,
+      ...spring,
+      ...black,
+      ...navy,
+      ...maroon,
+      ...pink,
+      ...white,
+      ...brown,
+    ]
+    // season=Winter&season=Summer&color=Black
+    console.log('test', this.state)
     // seperating products by type
     return (
       <div className="Landing">
+        <div>
+          <Link to="/products/add">Add Product</Link>
+        </div>
+
         <div className="checkbox">
+          <label>Select a season: </label>
           <label>
             <input
               type="checkbox"
               name="spring"
               onChange={this.toggleCheckboxChange}
+              value="Spring"
             />
-            Spring
+            Spring{' '}
           </label>
           <label>
-            <input
-              type="checkbox"
-              name="summer"
-              onChange={this.toggleCheckboxChange}
-            />
+            <input type="checkbox" name="summer" onChange={this.toggleCheckboxChange} />
             Summer
           </label>
           <label>
-            <input
-              type="checkbox"
-              name="fall"
-              onChange={this.toggleCheckboxChange}
-            />
+            <input type="checkbox" name="autumn" onChange={this.toggleCheckboxChange} />
             Autumn
           </label>
           <label>
-            <input
-              type="checkbox"
-              name="winter"
-              onChange={this.toggleCheckboxChange}
-            />
+            <input type="checkbox" name="winter" onChange={this.toggleCheckboxChange} />
             Winter
           </label>
         </div>
-        <div>
-          <Link to="/products/add">Add Product</Link>
+
+        <div className="checkbox">
+          <label>Select a color: </label>
+          <label>
+            <input type="checkbox" name="black" onChange={this.toggleCheckboxChange} />
+            Black
+          </label>
+          <label>
+            <input type="checkbox" name="navy" onChange={this.toggleCheckboxChange} />
+            Navy
+          </label>
+          <label>
+            <input type="checkbox" name="maroon" onChange={this.toggleCheckboxChange} />
+            Maroon
+          </label>
+          <label>
+            <input type="checkbox" name="pink" onChange={this.toggleCheckboxChange} />
+            Pink
+          </label>
+          <label>
+            <input type="checkbox" name="white" onChange={this.toggleCheckboxChange} />
+            White
+          </label>
+          <label>
+            <input type="checkbox" name="brown" onChange={this.toggleCheckboxChange} />
+            Brown
+          </label>
         </div>
+
         {this.filteredProducts.length
           ? this.filteredProducts.map(product => {
-            return (
-              <div className="SingleProduct" key={product.id}>
-                <div className="ProductText">
-                  <img src={product.imageUrl} />
-                  <h3>{product.name}</h3>
-                  <p>{product.color}</p>
+              return (
+                <div className="SingleProduct" key={product.id}>
+                  <div className="ProductText">
+                    <img src={product.imageUrl} />
+                    <h3>{product.name}</h3>
+                    <p>{product.color}</p>
+                    <p>{product.season}</p>
+                  </div>
                 </div>
-              </div>
-            )
-          })
+              )
+            })
           : products.map(product => {
-            return (
-              <div className="SingleProduct" key={product.id}>
-                <div className="ProductText">
-                  <img src={product.imageUrl} />
-                  <h3>{product.name}</h3>
-                  <p>{product.color}</p>
+              return (
+                <div className="SingleProduct" key={product.id}>
+                  <div className="ProductText">
+                    <img src={product.imageUrl} />
+                    <h3>{product.name}</h3>
+                    <p>{product.color}</p>
+                    <p>{product.season}</p>
+                  </div>
                 </div>
-              </div>
-            )
-          })}
+              )
+            })}
       </div>
     )
   }
