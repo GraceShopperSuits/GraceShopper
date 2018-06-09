fb95fb887c1333df73111801a0d9d67e613d116b
Nabil Yafai <nabilyafai90@gmail.com>
2018-06-09 16:40:57 -0400

    added mock photo to show a suit in all products view

--- client/components/product/allProducts.js
+++ client/components/product/allProducts.js
@@ -180,8 +180,8 @@ class AllProducts extends Component {
                       <div className="col s3" key={product.id}>
                         <div className="card">
                           <div className="card-image">
-                            <img src={product.imageUrl} />
-                            <a className="btn-floating halfway-fab waves-effect waves-light red">
+                            <img src="https://a.suitsupplycdn.com/image/upload/b_white,c_pad,dpr_2.0,f_auto,fl_progressive,h_517,q_auto,w_375/v1/products/Suits/default/P5462_1.jpg" />
+                            <a className="btn-floating halfway-fab waves-effect waves-light blue-grey darken-4">
                               <i className="material-icons">add_shopping_cart</i>
                             </a>
                           </div>
