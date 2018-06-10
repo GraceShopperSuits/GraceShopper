06dd97b4bd2137a17b26e0693c268f8878ebc25a
Nabil Yafai <nabilyafai90@gmail.com>
2018-06-10 17:27:24 -0400

    added link in landing page and fixed the buggy photo height/width, should be working fine now

--- client/components/home.js
+++ client/components/home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 import Materialize from '../../materialize/materialize.min'
 
 class Home extends Component {
@@ -19,9 +20,11 @@ class Home extends Component {
   }
   render() {
     return (
-      <div className="carousel carousel-slider center">
+      <div id="carousel" className="carousel carousel-slider center">
         <div className="carousel-fixed-item center">
-          <a className="btn waves-effect white grey-text darken-text-2">Shop Now</a>
+          <Link to="/products" className="btn waves-effect white grey-text darken-text-2">
+            Shop Now
+          </Link>
         </div>
         <div className="carousel-item white-text" href="#one!">
           <img
@@ -29,13 +32,13 @@ class Home extends Component {
             src="https://images.pexels.com/photos/999267/pexels-photo-999267.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
           />
         </div>
-        <div className="carousel-item white-text" href="#one!">
+        <div className="carousel-item white-text" href="#two!">
           <img
             className="responsive-img carousel-image"
             src="https://images.pexels.com/photos/1093926/pexels-photo-1093926.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
           />
         </div>
-        <div className="carousel-item white-text" href="#one!">
+        <div className="carousel-item white-text" href="#three!">
           <img
             className="responsive-img carousel-image"
             src="https://images.pexels.com/photos/1143793/pexels-photo-1143793.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
