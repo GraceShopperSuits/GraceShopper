011ca64fd63bf335e3db07efc4d1362df75c0b6d
rayqi <qi.raymond@gmail.com>
2018-06-11 16:30:58 -0400

    try catchem all

--- server/api/cart.js
+++ server/api/cart.js
@@ -4,30 +4,33 @@ module.exports = router
 
 // '/api/cart'
 router.post('/', async (req, res, next) => {
-  const userId = req.user.id
-  console.log('body', req.body)
-  //get user id from req.user object
+  try {
+    const userId = req.user.id
+    //get user id from req.user object
 
-  //find an open order that is pending or create a new one
-  const order = await Order.findOrCreate({
-    where: {
-      userId,
-      status: 'pending',
-    },
-  })
-  //get keys(product ids) from cart object
-  const cartArr = Object.keys(req.body)
-
-  //map the array to convert keys to an instance of that product
-  const cartItems = await Promise.all(
-    cartArr.map(eachProduct => {
-      const product = Product.findById(Number(eachProduct))
-      return product
+    //find an open order that is pending or create a new one
+    const order = await Order.findOrCreate({
+      where: {
+        userId,
+        status: 'pending',
+      },
     })
-  )
+    //get keys(product ids) from cart object
+    const cartArr = Object.keys(req.body)
 
-  //add products to associated order
-  console.log('order', cartItems)
-  await order[0].addProducts(cartItems)
-  res.status(201).send('Order created')
+    //map the array to convert keys to an instance of that product
+    const cartItems = await Promise.all(
+      cartArr.map(eachProduct => {
+        const product = Product.findById(Number(eachProduct))
+        return product
+      })
+    )
+
+    //add products to associated order
+    await order[0].addProducts(cartItems)
+    res.status(201).send('Order created')
+  }
+  catch (error) {
+    next(error)
+  }
 })
