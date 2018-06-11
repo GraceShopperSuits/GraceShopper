bddf07b9ad855fc92f960663d0fb568764f8dfe3
Nabil Yafai <nabilyafai90@gmail.com>
2018-06-11 17:59:08 -0400

    removed logs

--- client/components/stripe.js
+++ client/components/stripe.js
@@ -9,23 +9,18 @@ class Stripe extends React.Component {
     this.onToken = this.onToken.bind(this)
   }
 
-  //JSON.stringify()
   onToken(token) {
     axios
       .post('/save-stripe-token', token)
       .then(response => response.data)
       .then(data => {
         alert(`Thanks for shopping with us ${this.props.email}!`)
-        console.log('props', this.props)
         this.props.history.push('/')
       })
   }
 
-  // ...
-
   render() {
     return (
-      // ...
       <StripeCheckout
         token={this.onToken}
         stripeKey="pk_test_HWld5WnOMiogEDaG7uaDooeZ"
