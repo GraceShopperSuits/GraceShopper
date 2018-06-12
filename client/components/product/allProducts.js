d8c07a4258623ce9f5fd8fd2e093076e0661c760
Donghao Wu <donghao.wu.us@gmail.com>
2018-06-12 17:23:29 -0400

    add some test on frontend and added image routes

--- client/components/product/allProducts.js
+++ client/components/product/allProducts.js
@@ -208,7 +208,9 @@ class AllProducts extends Component {
                     <div className="col s3 card-parent" key={product.id}>
                       <div className="card">
                         <div className="card-image">
-                          <img className="responsive-img" src={product.imageUrl} />
+                          <Link to={`/products/${product.id}`}>
+                            <img className="responsive-img" src={product.imageUrl} />
+                          </Link>
                           <a
                             className="btn-floating halfway-fab waves-effect waves-light black"
                             onClick={() => {
