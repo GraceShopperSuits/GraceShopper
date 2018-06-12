e2a23175a9a7f6a8569f6c11f8bb0160cdde6061
Jack Lye <catncradle@gmail.com>
2018-06-12 12:57:14 -0400

    changed secret

--- client/constants/server.js
+++ client/constants/server.js
@@ -1,6 +1,6 @@
 const PAYMENT_SERVER_URL =
   process.env.NODE_ENV === 'production'
-    ? 'http://myapidomain.com/payment'
+    ? 'http://suitz.herokuapp.com/payment'
     : 'http://localhost:8080/api/payment'
 
 export default PAYMENT_SERVER_URL
